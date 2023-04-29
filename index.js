05d20b8a3e06acb5a3774d84ef10d68070e95ceb
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-04-30 01:37:57 +0600

    vercel

--- index.js
+++ index.js
@@ -2,17 +2,39 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const port = 3000
+const port = process.env.PORT || 3000
 
-const categories = require('./data/catagories.json')
+const categories = require('./data/catagories.json');
+const news = require('./data/News.json')
 
 app.use(cors());
 app.get('/', (req, res) => {
-  res.send('Dragon is Coming Sooooooooon bosss........')
+  res.send('Dragon is Coming Sooooooooon bosss ki koro........')
 })
- app.get('/categories', (req, res)=>{
-    res.send(categories);
- })
+app.get('/categories', (req, res) => {
+  res.send(categories);
+})
+
+app.get('/news', (req, res) => {
+  res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+  const id = req.params.id;
+  if (id == 0) {
+    res.send(news)
+  }
+  else {
+    const categoryNews = news.filter(n => n.category_id === id);
+    res.send(categoryNews)
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
